552d9ca71445c00119de9d037a9795dce750cf9e
benjaminli7 <47728809+benjaminli7@users.noreply.github.com>
2019-09-20 10:46:13 +0200

    Ajoutez des fichiers projet.

---
+++ ModelObjet/Condition.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace ModelObjet
+{
+    public class Condition
+    {
+        const int nbJours = 30;
+        // Permet de savoir si on a le droit d'être remboursé
+        // On l'est à condition que le nombre de jours ne dépasse pas 30 !
+        public static bool Valider(int unNbDeJours)
+        {
+            if(unNbDeJours < nbJours)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+        // Permet de renvoyer le montant MAX remboursé en fonction de la catégorie
+        public static int CalculerMontantMax(string uneCategorie)
+        {
+            if(uneCategorie == "Livres")
+            {
+                return 30;
+            }
+            else if(uneCategorie == "Jouets")
+            {
+                return 50;
+            }
+            else if(uneCategorie == "Informatique")
+            {
+                return 1000;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        // Permet de retourner le total remboursé en fonction de tous les critères
+        public static double CalculerMontantRembourse(int unNbDeJours, string uneCategorie, bool estMembre, string unEtat, int unPrix)
+        {
+            double prixFinal = 0;
+            if (Condition.Valider(unNbDeJours) == true)
+            {
+                if(unPrix > CalculerMontantMax(uneCategorie))
+                {
+                    
+                    double reductionMembre = CalculerReductionMembre(estMembre);
+                    double reduction = CalculerReduction(unEtat);
+                    prixFinal = CalculerMontantMax(uneCategorie) * (1 - (reductionMembre + reduction));
+                    return prixFinal;
+                }
+                prixFinal = unPrix;
+                return prixFinal;
+            }
+            else
+            {
+                return prixFinal;
+            }
+        }
+        // Permet de renvoyer la réduction si on est membre ou pas
+        public static double CalculerReductionMembre(bool estMembre)
+        {
+            double reductionMembre;
+            if (estMembre)
+            {
+                reductionMembre = 0;
+            }
+            else
+            {
+                reductionMembre = 0.20;
+            }
+            return reductionMembre;
+        }
+        // Permet de renvoyer la réduction en fonction de l'état de l'achat
+        public static double CalculerReduction(string unEtat)
+        {
+            double reduction;
+            if (unEtat == "Abimé" || unEtat == "Très abimé")
+            {
+                reduction = 0.30;
+            }
+            else
+            {
+                reduction = 0.10;   
+            }
+            return reduction;
+        }
+    }
+}
---
+++ ProjetDeTests/UnitTest1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelObjet;
+
+namespace ProjetDeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void ValiderTest()
+        {
+
+            // Le nombre de jours d'achat est < à 30 jours
+            bool a = Condition.Valider(25);
+            Assert.AreEqual(true, a);
+
+            // Le nombre de jours d'achat est > à 30 jours
+            bool b = Condition.Valider(50);
+            Assert.AreEqual(false, b);
+        }
+
+        [TestMethod()]
+        public void CalculerMontantMaxTest()
+        {
+            // Pour la catégorie livre
+            int a = Condition.CalculerMontantMax("Livres");
+            Assert.AreEqual(30, a);
+
+            // Pour la catégorie jouet
+            int b = Condition.CalculerMontantMax("Jouets");
+            Assert.AreEqual(50, b);
+
+            // Pour la catégorie informatique
+            int c = Condition.CalculerMontantMax("Informatique");
+            Assert.AreEqual(1000, c);
+        }
+
+        [TestMethod()]
+        public void CalculerMontantRembourseTest()
+        {
+            // Un livre achété 24 euros depuis 15 jours avec un état "Très abimé" en étant non membre
+            double a = Condition.CalculerMontantRembourse(15, "Livres", false, "Très abimé", 24);
+            Assert.AreEqual(24, a);
+            // Un livre achété 24 euros depuis 15 jours avec un état "Bon" en étant membre
+            double b = Condition.CalculerMontantRembourse(15, "Livres", true, "Bon", 24);
+            
+
+            double c = Condition.CalculerMontantRembourse(20, "Livres", true, "Abimé", 50);
+            Assert.AreEqual(21, c);
+        }
+
+        [TestMethod()]
+        public void CalculerReductionMembreTest()
+        {
+            // Il est membre
+            double a = Condition.CalculerReductionMembre(true);
+            Assert.AreEqual(0, a);
+
+            // Il n'est pas membre
+            double b = Condition.CalculerReductionMembre(false);
+            Assert.AreEqual(0.2, b);
+
+        }
+
+        [TestMethod()]
+        public void CalculerReductionTest()
+        {
+            // Pour un état "bon"
+            double a = Condition.CalculerReduction("Bon");
+            Assert.AreEqual(0.1, a);
+
+            // Pour un état "abimé"
+            double b = Condition.CalculerReduction("Abimé");
+            Assert.AreEqual(0.3, b);
+            
+
+        }
+    }
+}
